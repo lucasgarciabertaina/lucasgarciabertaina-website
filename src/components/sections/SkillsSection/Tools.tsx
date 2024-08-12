e6ab9a6ff154fe16709d41b92727ddca0d9f86c1
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2024-08-12 16:50:46 +1200

    Adding some missing tools

--- src/components/sections/SkillsSection/Tools.tsx
+++ src/components/sections/SkillsSection/Tools.tsx
@@ -5,14 +5,18 @@ const Tools = () => (
     <Tool image="./typescript.svg"> Typescript</Tool>
     <Tool image="./python.svg"> Python</Tool>
     <Tool image="./nodejs.svg"> NodeJS</Tool>
+    <Tool image="./express.svg"> Express</Tool>
     <Tool image="./nestJS.svg"> NestJS</Tool>
-    <Tool image="./MongoDB.svg"> Mongo DB</Tool>
+    <Tool image="./mongoDB.svg"> Mongo DB</Tool>
+    <Tool image="./postgreSQL.svg"> PostgreSQL</Tool>
     <Tool image="./awsDynamoDB.svg"> Amazon DynamoDB</Tool>
     <Tool image="./awsSES.svg"> Amazon SES</Tool>
     <Tool image="./jest.svg"> Jest</Tool>
     <Tool image="./docker.svg"> Docker</Tool>
     <Tool image="./vue.svg"> Vue</Tool>
+    <Tool image="./nuxt.svg"> Nuxt</Tool>
     <Tool image="./react.svg"> React</Tool>
+    <Tool image="./next.svg"> Nextjs</Tool>
     <Tool image="./cypress.svg"> Cypress</Tool>
   </div>
 );
