9fc6257aa800a7e228723eb5c0ae3f172e26f594
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2024-08-12 16:50:56 +1200

    Finishing Experience Section

--- src/components/sections/ExperiencesSection/Tracestory.tsx
+++ src/components/sections/ExperiencesSection/Tracestory.tsx
@@ -2,20 +2,21 @@ import Bulleted from "@/components/ui/BulletedItem";
 
 const Tracestory = () => (
   <div className="mt-12 p-8 w-3/4 bg-white mx-auto grid grid-cols-3 grid-rows-1">
-    <img src="./logo-upwork.png" alt="logo" className="w-12 h-12" />
+    <img src="./tracestory.svg" alt="logo" />
     <div>
       <h4 className="text-gray-900 text-xl font-semibold pb-4">
         Fullstack Developer
       </h4>
       <Bulleted>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Responsible for refactoring the web application from scratch.
       </Bulleted>
+      <Bulleted>Co-creator of the BI platform.</Bulleted>
       <Bulleted>
-        Ut pretium arcu et massa semper, id fringilla leo semper.
+        Designed and implemented the QR, one of the core utility of the platform
       </Bulleted>
-      <Bulleted>Sed quis justo ac magna.</Bulleted>
+      <Bulleted>Created used AWS SES email sending process.</Bulleted>
       <Bulleted>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        One of the responsible for server maintenance and bug fixes
       </Bulleted>
     </div>
     <p className="text-gray-700 text-base font-normal text-right">
